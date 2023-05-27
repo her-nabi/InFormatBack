41b48acc0b35e6d02f0b0197c71cd793eb1b1ff1
Абдуллаева Надежда <abdullaeva.nadya494a@gmail.com>
2023-05-27 18:55:37 +0700

    Initial commit

---
+++ src/main/java/ru/abdullaeva/informatbackend/InFormatBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.abdullaeva.informatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InFormatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InFormatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/abdullaeva/informatbackend/InFormatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abdullaeva.informatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InFormatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
