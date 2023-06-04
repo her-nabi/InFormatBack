ceae5ac868f132eee06d346d1b44dd9f287a65a0
Абдуллаева Надежда <abdullaeva.nadya494a@gmail.com>
2023-06-05 00:06:07 +0700

    Change Role entity.

--- src/main/java/ru/abdullaeva/informatbackend/model/auth/Role.java
+++ src/main/java/ru/abdullaeva/informatbackend/model/auth/Role.java
@@ -13,7 +13,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Entity
